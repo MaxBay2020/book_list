5cfdaaecba138daf5f2b948e14a8d13fa515c7c7
Cong Wang <cwang167@my.centennialcollege.ca>
2024-03-23 20:42:03 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+'use client'
 import styles from './page.module.css'
+import {useRouter} from "next/navigation";
 
 const HomePage = () => {
+
+    const router = useRouter()
+
+    const gotoBooksPage = () => {
+        router.push('/books')
+    }
+
   return (
-      <p className={styles.container}>
-        home page
-      </p>
+      <section className={styles.container}>
+          <p >
+              home page
+          </p>
+          <button className={styles.gotoButton} onClick={() => gotoBooksPage()}>
+              Go to book list page
+          </button>
+
+      </section>
   );
 };
 
